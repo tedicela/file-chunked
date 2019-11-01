71d0ddb1bdb4b8db4f7d1dceecfe3a689aad6f8f
4X M <tedicela@gmail.com>
2019-11-02 00:28:46 +0100

    Clear old or orphaned chunks

--- FileChunked.js
+++ FileChunked.js
@@ -16,7 +16,6 @@ class FileChunked {
 	}
 	mkdirRecursive(path){
 		const DS = this.getDS();
-		
 		const folders = path.split(DS);
 		
 		let checkPath = "";
@@ -30,32 +29,45 @@ class FileChunked {
 			if (!fs.existsSync(checkPath)){
 				fs.mkdirSync(checkPath);
 			}
+		}		
+	}
+	getChunkStoragePath(fileChunkStorage){
+		const DS = this.getDS();
+		let CHUNK_STORAGE = fileChunkStorage.split(DS);
+		if(CHUNK_STORAGE[CHUNK_STORAGE.length-1] == ""){
+			CHUNK_STORAGE[CHUNK_STORAGE.length-1] = 'tmp_chunks';
+		}else{
+			CHUNK_STORAGE.push('tmp_chunks');
 		}
+		CHUNK_STORAGE = CHUNK_STORAGE.join(DS);
 		
+		return CHUNK_STORAGE;
 	}
 	upload(file){
 		
-		if (!fs.existsSync(file.chunkStorage)){
-			this.mkdirRecursive(file.chunkStorage);
+		let CHUNK_STORAGE = this.getChunkStoragePath(file.chunkStorage);
+		
+		if (!fs.existsSync(CHUNK_STORAGE)){
+			this.mkdirRecursive(CHUNK_STORAGE);
 		}
 		
 		let oldTmpFileName = file.filePath;
 		if(file.chunkIndex < file.totalChunksCount - 1){ //merge
 			
 			if(file.chunkIndex > 0){ // do merge
-				oldTmpFileName = file.chunkStorage + ["chunk", file.uploadId, file.chunkIndex-1].join("-");
+				oldTmpFileName = CHUNK_STORAGE + ["chunk", file.uploadId, file.chunkIndex-1].join("-");
 				let chunkedFile = fs.readFileSync(file.filePath);
 				fs.appendFileSync(oldTmpFileName, chunkedFile);
 				fs.unlinkSync(file.filePath);
 			}
 			
-			let newTmpFileName = file.chunkStorage + ["chunk", file.uploadId, file.chunkIndex].join("-");
+			let newTmpFileName = CHUNK_STORAGE + ["chunk", file.uploadId, file.chunkIndex].join("-");
 			//Rename and move file:
 			fs.renameSync(oldTmpFileName, newTmpFileName);
 			
 		}else{
 			//Final merge a rename it as final fileName:
-			oldTmpFileName = file.chunkStorage + ["chunk", file.uploadId, file.chunkIndex-1].join("-");
+			oldTmpFileName = CHUNK_STORAGE + ["chunk", file.uploadId, file.chunkIndex-1].join("-");
 			let chunkedFile = fs.readFileSync(file.filePath);
 			fs.appendFileSync(oldTmpFileName, chunkedFile);
 			fs.unlinkSync(file.filePath);
@@ -63,6 +75,27 @@ class FileChunked {
 			//Rename and move file:	
 			fs.renameSync(oldTmpFileName, file.filePath);
 		}
+		this.clearOrphanedChunks(CHUNK_STORAGE);
+	}
+	clearOrphanedChunks(storagePath){
+		const DS = this.getDS();
+		fs.readdir(storagePath, function (err, files) {
+			//handling error
+			if (err) {
+				return console.log('Unable to scan directory: ' + err);
+			} 
+			//listing all files using forEach
+			files.forEach(function (file) {
+				if( file.split("-")[0] != 'chunk') return;
+				
+				let file_path = (storagePath[storagePath.length-1] != DS ) ? storagePath+DS+file : storagePath+file;
+				fs.stat(file_path, function(err, stats){
+					if( (new Date()).getTime() - stats.mtime.getTime() >= 24*60*60*1000 ){
+						fs.unlinkSync(file_path);
+					}
+				});
+			});
+		});
 	}
 }
 module.exports = new FileChunked();
